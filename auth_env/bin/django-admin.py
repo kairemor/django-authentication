ccf207f6a9f961905ac7ffdf365ccfe31314b54e
kmtc44 <kairmor@yahoo.com>
2020-09-11 22:43:10 +0000

    django authentication

---
+++ auth_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/kairemor/Documents/Python/FrameWork/Django/Program/Authentification/mysite/auth_env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
